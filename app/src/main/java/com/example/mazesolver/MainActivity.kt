4c8ec9ee4d48709269efab61cc41b633034124cc
Mohamadhassan <emohamadhassan@gmail.com>
2020-01-10 17:40:47 +0330

    Added Node

--- app/src/main/java/com/example/mazesolver/MainActivity.kt
+++ app/src/main/java/com/example/mazesolver/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.mazesolver
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
