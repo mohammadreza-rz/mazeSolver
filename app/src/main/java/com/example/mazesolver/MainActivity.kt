d96857cc15c52690bd8236f6bf63a02f5456a4fc
smrz <mr.razavi77@gmail.com>
2020-01-19 20:54:09 +0330

    bfs bug fixed and tested

--- app/src/main/java/com/example/mazesolver/MainActivity.kt
+++ app/src/main/java/com/example/mazesolver/MainActivity.kt
@@ -1,33 +1,58 @@
 package com.example.mazesolver
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     private val dimensionSize = 20
     private val tag = "check_tag"
-    private var start = Node(Status.Empty, Coordinate(0, 0), null)
-    private var goal = Node(Status.Empty, Coordinate(4, 4), null)
+    private var start = Node(Status.Empty, Coordinate(1, 1), null)
+    private var goal = Node(Status.Empty, Coordinate(12, 19), null)
     private val gameBoard = Array(dimensionSize) { i ->
         Array(dimensionSize) { j ->
             Node(Status.Empty, Coordinate(i, j), null)
         }
     }
-    private val frontier = mutableListOf<Node>()
-    private val exploredNodes = mutableSetOf<Node>()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-//        Log.d(tag, findPossibleMoves(0, 0).toString())
+        gameBoard[3][1].nodeStatus = Status.Block
+        gameBoard[2][2].nodeStatus = Status.Block
+        findRouteByBFS()?.forEach { Log.d(tag, it.coordinate.toString()) }
     }
 
+    private fun findRouteByBFS(): List<Node>? {
+        val frontier = mutableListOf<Node>()
+        val exploredNodes = mutableSetOf<Node>()
+        frontier.add(start)
+        while (true) {
+            if (frontier.isEmpty())
+                return null
+            val selectNode = frontier.removeAt(0)
+            if (isGoal(selectNode)) {
+                return computeRouting(selectNode)
+            }
+            exploredNodes += selectNode
+            val possibleMoves = findPossibleMoves(selectNode)
+            frontier += possibleMoves.filterNot { exploredNodes.any { hadMoved -> hadMoved.coordinate == it.coordinate } }
+                .filterNot { frontier.any { willMove -> willMove.coordinate == it.coordinate } }
+        }
+    }
+
+    private fun findRouteByDepthLimitedSearch(depthLimit: Int): List<Node>? {
+        if (isGoal(start))
+            return computeRouting(start)
+
+        return null
+    }
+
+
     private fun findPossibleMoves(node: Node): List<Node> {
-        val x = node.coordinate.first
-        val y = node.coordinate.second
+        val (x, y) = node.coordinate
         val possibleMoves = mutableListOf<Node>()
         if (x + 1 != dimensionSize && gameBoard[x + 1][y].nodeStatus != Status.Block)
             possibleMoves += Node(Status.Empty, Coordinate(x + 1, y), node)
@@ -37,35 +62,10 @@ class MainActivity : AppCompatActivity() {
             possibleMoves += Node(Status.Empty, Coordinate(x - 1, y), node)
         if (y - 1 != -1 && gameBoard[x][y - 1].nodeStatus != Status.Block)
             possibleMoves += Node(Status.Empty, Coordinate(x, y - 1), node)
-
         return possibleMoves
     }
 
-    private fun BFSsearch(): List<Node>? {
-        frontier += start
-        while (true) {
-            if (frontier.isEmpty())
-                return null
-            val selectNode = frontier[0]
-            frontier.removeAt(0)
-            if (isGoal(selectNode))
-                computeRouting(selectNode)
-            exploredNodes += selectNode
-            val possibleMoves = findPossibleMoves(selectNode)
-            possibleMoves.forEach {
-                if (it !in exploredNodes && it !in frontier)
-                    frontier.add(it)
-            }
-        }
-    }
-
-    private fun isGoal(node: Node): Boolean {
-        val nodeCoordinate = node.coordinate
-        val goalCoordinate = goal.coordinate
-        if (nodeCoordinate.first == goalCoordinate.first && nodeCoordinate.second == goalCoordinate.second)
-            return true
-        return false
-    }
+    private fun isGoal(node: Node) = node.coordinate == goal.coordinate
 
     private fun computeRouting(node: Node): List<Node> {
         val routeToGoal = mutableListOf<Node>()
