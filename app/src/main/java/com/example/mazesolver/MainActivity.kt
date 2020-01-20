180397219376fe09b60f7b3bc544f907061418ec
smrz <mr.razavi77@gmail.com>
2020-01-20 10:27:02 +0330

    A Star Algorithm added

--- app/src/main/java/com/example/mazesolver/MainActivity.kt
+++ app/src/main/java/com/example/mazesolver/MainActivity.kt
@@ -3,16 +3,22 @@ package com.example.mazesolver
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import kotlin.math.hypot
 
 class MainActivity : AppCompatActivity() {
     private val dimensionSize = 20
     private val tag = "check_tag"
     private var start = Node(Status.Empty, Coordinate(1, 1), null)
-    private var goal = Node(Status.Empty, Coordinate(12, 19), null)
+    private var goal = Node(Status.Empty, Coordinate(18, 19), null)
     private val gameBoard = Array(dimensionSize) { i ->
         Array(dimensionSize) { j ->
             Node(Status.Empty, Coordinate(i, j), null)
         }
+    }
+    private val DFSVisitedNodes = mutableListOf<Node>()
+
+    enum class SolutionStatus {
+        Failure, Cutoff, Success
     }
 
 
@@ -22,7 +28,80 @@ class MainActivity : AppCompatActivity() {
 
         gameBoard[3][1].nodeStatus = Status.Block
         gameBoard[2][2].nodeStatus = Status.Block
-        findRouteByBFS()?.forEach { Log.d(tag, it.coordinate.toString()) }
+//        gameBoard[7][5].nodeStatus = Status.Block
+//        gameBoard[10][5].nodeStatus = Status.Block
+//        gameBoard[9][12].nodeStatus = Status.Block
+//        gameBoard[19][15].nodeStatus = Status.Block
+//        gameBoard[12][15].nodeStatus = Status.Block
+//        gameBoard[5][6].nodeStatus = Status.Block
+//        findRouteByBFS()?.forEach { Log.d(tag, it.coordinate.toString()) }
+//        iterativeDeepeningSearch().forEach {
+//            Log.d(
+//                tag,
+//                "/>${it.coordinate}"
+//            )
+//        }
+        aStarSearch().forEach { Log.d(tag, "/>${it.coordinate}") }
+    }
+
+    private fun aStarSearch(): List<Node> {
+        val frontier = mutableMapOf(start to calculateFValue(start))
+        while (true) {
+            val bestChoice = frontier.minBy { it.value }!!.key
+            frontier.remove(bestChoice)
+            if (bestChoice == goal)
+                return computeRoutingToStart(bestChoice).first
+            Log.d(tag, "*>${bestChoice.coordinate}")
+            findPossibleMoves(bestChoice).filterNot { it in frontier.keys }
+                .forEach {
+                    frontier += it to calculateFValue(it)
+                }
+        }
+    }
+
+    private fun iterativeDeepeningSearch(): List<Node> {
+
+        fun findRouteByDepthLimitedSearch(
+            node: Node,
+            limit: Int
+        ): Pair<SolutionStatus, List<Node>> {
+            DFSVisitedNodes += (node)
+//            Log.d(tag, "***********************->${DFSVisitedNodes.size}")
+            when {
+                isGoal(node) -> return SolutionStatus.Success to listOf(node)
+                limit == 0 -> return SolutionStatus.Cutoff to listOf()
+                else -> {
+                    var cutoffOccurred = false
+                    findPossibleMoves(node)
+                        .filterNot { it in DFSVisitedNodes }
+                        .forEach {
+                            val result = findRouteByDepthLimitedSearch(it, limit - 1)
+                            DFSVisitedNodes.removeAt(DFSVisitedNodes.lastIndex)
+//                            Log.d(tag, ">${result.first} at ${it.coordinate}")
+                            if (result.first == SolutionStatus.Cutoff) {
+                                cutoffOccurred = true
+                            } else if (result.first != SolutionStatus.Failure)
+                                return result.first to listOf(it, *result.second.toTypedArray())
+                        }
+                    return if (cutoffOccurred) {
+                        SolutionStatus.Cutoff to listOf()
+                    } else {
+//                        Log.d(tag, "f at ${node.coordinate}")
+                        SolutionStatus.Failure to listOf()
+                    }
+                }
+            }
+        }
+
+        repeat(40) {
+            Log.d(tag, "->$it")
+            val (state, result) = findRouteByDepthLimitedSearch(start, it)
+            if (state == SolutionStatus.Success) {
+                return result
+            } else
+                DFSVisitedNodes.clear()
+        }
+        return listOf()
     }
 
     private fun findRouteByBFS(): List<Node>? {
@@ -34,20 +113,13 @@ class MainActivity : AppCompatActivity() {
                 return null
             val selectNode = frontier.removeAt(0)
             if (isGoal(selectNode)) {
-                return computeRouting(selectNode)
+                return computeRoutingToStart(selectNode).first
             }
             exploredNodes += selectNode
             val possibleMoves = findPossibleMoves(selectNode)
-            frontier += possibleMoves.filterNot { exploredNodes.any { hadMoved -> hadMoved.coordinate == it.coordinate } }
-                .filterNot { frontier.any { willMove -> willMove.coordinate == it.coordinate } }
+            frontier += possibleMoves.filterNot { it in exploredNodes }
+                .filterNot { it in frontier }
         }
-    }
-
-    private fun findRouteByDepthLimitedSearch(depthLimit: Int): List<Node>? {
-        if (isGoal(start))
-            return computeRouting(start)
-
-        return null
     }
 
 
@@ -67,15 +139,28 @@ class MainActivity : AppCompatActivity() {
 
     private fun isGoal(node: Node) = node.coordinate == goal.coordinate
 
-    private fun computeRouting(node: Node): List<Node> {
-        val routeToGoal = mutableListOf<Node>()
-        routeToGoal.add(node)
+    private fun computeRoutingToStart(node: Node): Pair<List<Node>, Int> {
+        val routeNodeToStart = mutableListOf<Node>()
+        var gValue = 1
+        routeNodeToStart.add(node)
         var parentNode = node.parent
         while (parentNode != null) {
-            routeToGoal.add(parentNode)
+            routeNodeToStart.add(parentNode)
             parentNode = parentNode.parent
+            gValue++
         }
-        return routeToGoal
+        return routeNodeToStart to gValue
+    }
+
+    private fun calculateFValue(node: Node): Double {
+        return calculateHeuristicsValue(node) + computeRoutingToStart(node).second
+    }
+
+    private fun calculateHeuristicsValue(node: Node): Double {
+        return hypot(
+            (node.coordinate.first - goal.coordinate.first).toDouble(),
+            (node.coordinate.second - node.coordinate.second).toDouble()
+        )
     }
 
 
--- app/src/main/java/com/example/mazesolver/Node.kt
+++ app/src/main/java/com/example/mazesolver/Node.kt
@@ -15,4 +15,21 @@ class Node(nodeStatus: Status, val coordinate: Coordinate, val parent: Node?) {
         }
 
     var nodeBackgroundColor: Int = -1
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Node
+
+        if (coordinate != other.coordinate) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return coordinate.hashCode()
+    }
+
+
 }
